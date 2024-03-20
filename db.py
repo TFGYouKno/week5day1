7bc0e2eff48c8e57f18fd5d60f0fe4665d78ed07
TFGYouKno <brockhoffharrison@comcast.net>
2024-03-19 21:28:46 -0400

    waymorecomplicatedthanitneededtobe

---
+++ db.py
@@ -0,0 +1,27 @@
+# Temp! do not need after we connect to database
+
+users = {
+    1:{
+        'id' : 1,
+        'username' : 'bc',
+        'email' : 'bc@ct.com'
+    },
+    2 : {
+        'id' : 2,
+        'username' : 'ds',
+        'email' : 'ds@ct.com'
+    }
+}
+
+posts = {
+    '1' : {
+        'author' : 2,
+        'title' : 'Welcome to Flask',
+        'body' : 'Welcome to our first full-stack app, it\'s SUPER easy and simple!'
+    },
+    '2' : {
+        'author' : 1,
+        'title' : 'Flask intro',
+        'body' : 'I wouldn\'t say it is THAT simple, but i am dumb as hell'
+    }
+}
